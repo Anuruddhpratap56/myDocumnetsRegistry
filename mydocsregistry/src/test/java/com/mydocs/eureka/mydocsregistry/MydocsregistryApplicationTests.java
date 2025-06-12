da4562f946d0097bfa18ac649ad59665e222fa0e
Bhanu Pratap Singh <you@example.com>
2025-06-12 07:45:56 +0530

    first commit

---
+++ mydocsregistry/src/test/java/com/mydocs/eureka/mydocsregistry/MydocsregistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mydocs.eureka.mydocsregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydocsregistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
